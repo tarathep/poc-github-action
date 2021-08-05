9bb25efa171ed40edd6ac01261b9f6fdb739d688
tarathep <bokie.tarathep@gmail.com>
2021-08-05 14:23:41 +0700

    change message for active pipline

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController{
 
     @GetMapping("/hello")
 	public String Hello(){
-		return "hello web app with JAVA Spring Framework :D";
+		return "hello web app with JAVA Spring Framework :D 2";
 	}
 }
